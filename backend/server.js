be1d60dee2a7d57988e97cae2632aa061180795c
Bian Péter <bianpeti@gmail.com>
2021-03-09 13:20:01 +0100

    adding api key

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const NewsAPI = require("newsapi");
-const newsapi = new NewsAPI("42232197917242eeb407d032926b3532");
+const newsapi = new NewsAPI("7d93f33879054c1a9b9f2741a91e12bd"); //7d93f33879054c1a9b9f2741a91e12bd //42232197917242eeb407d032926b3532
 const PORT = 8000;
 
 app.use(cors());
