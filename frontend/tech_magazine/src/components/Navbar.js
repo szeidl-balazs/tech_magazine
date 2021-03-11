8cab68a72ca7eceb4d08512f23d9fb80482353da
Bian Péter <bianpeti@gmail.com>
2021-03-11 19:00:06 +0100

    navbar content fix

--- frontend/tech_magazine/src/components/Navbar.js
+++ frontend/tech_magazine/src/components/Navbar.js
@@ -5,13 +5,10 @@ function Navbar() {
   return (
     <div className="navbar-conatiner">
       <div className="navbar">
-        <div>
-        <div className=""><img src={logo3} alt="logo3"></img></div>
-        </div>
+        <img src={logo3} alt="logo3"></img>
         <h4>About</h4>
-        <h4>v</h4>
-        <h4>v</h4>
         <h4>Community</h4>
+        <h4 className="right">Login</h4>
       </div>
     </div>
   )
