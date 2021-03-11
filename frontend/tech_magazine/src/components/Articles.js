d0e27e8f373cb645948e02bf7f1820ac59e2a47a
Bian Péter <bianpeti@gmail.com>
2021-03-11 22:02:17 +0100

    navbar and wall css fix

--- frontend/tech_magazine/src/components/Articles.js
+++ frontend/tech_magazine/src/components/Articles.js
@@ -2,7 +2,7 @@ function Articles({ news }) {
   return (
       <div className="article-container">
         <h1>BETYÁR NEWS</h1>
-        <h2 className="fl">Technology</h2>
+        <h2 className="fl">Technology Articles</h2>
         {news.data.articles.map((article, index) => (
           <div className="article" key={index}>
             <a href={article.url} rel="noreferrer" target="_blank"><img className="image" src={article.urlToImage} alt="Pics is Not Found, please visit the website for more info" /></a>
--- frontend/tech_magazine/src/components/Navbar.js
+++ frontend/tech_magazine/src/components/Navbar.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import logo3 from "../logo/logo3.svg"
+import {Link} from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar-conatiner">
       <div className="navbar">
         <img src={logo3} alt="logo3"></img>
-        <h4>All Articles</h4>
-        <h4>Tech Articles</h4>
-        <h4 className="right">Login</h4>
+        <Link className="link1" to="/">
+          <h4>All Articles</h4>
+        </Link>
+        <Link className="link1" to="/">
+          <h4>Tech Articles</h4>
+        </Link>
+        <Link className="link1" to="/">
+          <h4 className="right">Login</h4>
+        </Link>
       </div>
     </div>
   )
